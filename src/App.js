0d7e972419458067e3ff8cdff797ab4a5e5b18f5
chris <kokosias@yahoo.gr>
2020-07-26 18:23:13 +0300

    add editor

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { EditoriaLayout } from "wax-prosemirror-layouts";
+import { Wax } from "wax-prosemirror-core";
+
+import config from "./config";
+import text from "./text";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    overflow-y: hidden;
+  #root {
+    height:100vh;
+    width:100vw;
+  }
+  }
+`;
+
+const StyledWax = styled(Wax)`
+  .wax-surface-scroll {
+    /* height: ${(props) => (props.debug ? "50vh" : "100%")}; */
+  }
+`;
+
+const renderImage = (file) => {
+  const reader = new FileReader();
+  return new Promise((accept, fail) => {
+    reader.onload = () => accept(reader.result);
+    reader.onerror = () => fail(reader.error);
+    // Some extra delay to make the asynchronicity visible
+    setTimeout(() => reader.readAsDataURL(file), 150);
+  });
+};
+
+const user = {
+  userId: "1234",
+  username: "demo",
+};
+
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <StyledWax
+      config={config}
+      autoFocus
+      placeholder="Type Something..."
+      fileUpload={(file) => renderImage(file)}
+      value={text}
+      layout={EditoriaLayout}
+      // onChange={(source) => console.log(source)}
+      user={user}
+    />
+  </Fragment>
+);
 
 export default App;
