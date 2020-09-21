7798bd89bfaac296fa18215e6f218eb55d753e70
chris <kokosias@yahoo.gr>
2020-09-21 16:04:59 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/editor-demo/precache-manifest.cb692eb0d638404472ee555dd328d2fc.js"
+  "/editor-demo/precache-manifest.f16d1fc79eab75031dc531644a7b4ea9.js"
 );
 
 self.addEventListener('message', (event) => {
